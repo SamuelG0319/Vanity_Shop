eccdb7f287bd960b94a24c6d607fabb04185cf06
samuel <samuel.lasso@utp.ac.pa>
2023-12-08 22:17:59 -0500

    Auto stash before merge of "main" and "origin/main"

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
+
+    <?php
+    require_once('dbconn.php');
+    ?>
+
     <meta charset="UTF-8">
     <meta name="description" content="">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,10 +14,14 @@
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Title  -->
-    <title>Essence - Fashion Ecommerce Template</title>
+    <title>Vanity - Tu tienda de ropa</title>
 
     <!-- Favicon  -->
+<<<<<<< Updated upstream:index.html
     <link rel="icon" href="assets/img/core-img/favicon.ico">
+=======
+    <link rel="icon" href="assets/img/core-img/logo.png">
+>>>>>>> Stashed changes:index.php
 
     <!-- Core Style CSS -->
     <link rel="stylesheet" href="assets/css/core-style.css">
@@ -27,7 +36,11 @@
             <!-- Classy Menu -->
             <nav class="classy-navbar" id="essenceNav">
                 <!-- Logo -->
+<<<<<<< Updated upstream:index.html
                 <a class="nav-brand" href="index.html"><img src="assets/img/core-img/logo.png" alt=""></a>
+=======
+                <a class="nav-brand" href="index.php"><img src="assets/img/core-img/logo.png" alt="" height="100px" width="100px"></a>
+>>>>>>> Stashed changes:index.php
                 <!-- Navbar Toggler -->
                 <div class="classy-navbar-toggler">
                     <span class="navbarToggler"><span></span><span></span><span></span></span>
@@ -41,6 +54,7 @@
                     <!-- Nav Start -->
                     <div class="classynav">
                         <ul>
+<<<<<<< Updated upstream:index.html
                             <li><a href="#">Shop</a>
                                 <div class="megamenu">
                                     <ul class="single-mega cn-col-4">
@@ -86,6 +100,12 @@
                             </li>
                             <li><a href="blog.html">Blog</a></li>
                             <li><a href="contact.html">Contact</a></li>
+=======
+                            <li><a href="tops.php">Partes de arriba</a></li>
+                            <li><a href="bottom.php">Partes de abajo</a></li>
+                            <li><a href="shoes.php">Zapatos</a></li>
+                            <li><a href="accesories.php">Accesorios</a></li>
+>>>>>>> Stashed changes:index.php
                         </ul>
                     </div>
                     <!-- Nav End -->
@@ -94,6 +114,7 @@
 
             <!-- Header Meta Data -->
             <div class="header-meta d-flex clearfix justify-content-end">
+<<<<<<< Updated upstream:index.html
                 <!-- Search Area -->
                 <div class="search-area">
                     <form action="#" method="post">
@@ -105,6 +126,8 @@
                 <div class="favourite-area">
                     <a href="#"><img src="assets/img/core-img/heart.svg" alt=""></a>
                 </div>
+=======
+>>>>>>> Stashed changes:index.php
                 <!-- User Login Info -->
                 <div class="user-login-info">
                     <a href="#"><img src="assets/img/core-img/user.svg" alt=""></a>
@@ -139,7 +162,7 @@
                         <img src="assets/img/product-img/product-1.jpg" class="cart-thumb" alt="">
                         <!-- Cart Item Desc -->
                         <div class="cart-item-desc">
-                          <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
+                            <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
                             <span class="badge">Mango</span>
                             <h6>Button Through Strap Mini Dress</h6>
                             <p class="size">Size: S</p>
@@ -155,7 +178,7 @@
                         <img src="assets/img/product-img/product-2.jpg" class="cart-thumb" alt="">
                         <!-- Cart Item Desc -->
                         <div class="cart-item-desc">
-                          <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
+                            <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
                             <span class="badge">Mango</span>
                             <h6>Button Through Strap Mini Dress</h6>
                             <p class="size">Size: S</p>
@@ -171,7 +194,7 @@
                         <img src="assets/img/product-img/product-3.jpg" class="cart-thumb" alt="">
                         <!-- Cart Item Desc -->
                         <div class="cart-item-desc">
-                          <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
+                            <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
                             <span class="badge">Mango</span>
                             <h6>Button Through Strap Mini Dress</h6>
                             <p class="size">Size: S</p>
@@ -206,9 +229,9 @@
             <div class="row h-100 align-items-center">
                 <div class="col-12">
                     <div class="hero-content">
-                        <h6>asoss</h6>
-                        <h2>New Collection</h2>
-                        <a href="#" class="btn essence-btn">view collection</a>
+                        <h5>micasa</h5>
+                        <h2>Vanity Shop</h2>
+                        <h6>Tu tienda de ropa online</h6>
                     </div>
                 </div>
             </div>
@@ -220,27 +243,36 @@
     <div class="top_catagory_area section-padding-80 clearfix">
         <div class="container">
             <div class="row justify-content-center">
-                <!-- Single Catagory -->
+
+                <!-- Single Category -->
                 <div class="col-12 col-sm-6 col-md-4">
                     <div class="single_catagory_area d-flex align-items-center justify-content-center bg-img" style="background-image: url(assets/img/bg-img/bg-2.jpg);">
                         <div class="catagory-content">
-                            <a href="#">Clothing</a>
+                            <a href="#">Partes de arriba</a>
                         </div>
                     </div>
                 </div>
-                <!-- Single Catagory -->
+                <!-- Single Category -->
                 <div class="col-12 col-sm-6 col-md-4">
                     <div class="single_catagory_area d-flex align-items-center justify-content-center bg-img" style="background-image: url(assets/img/bg-img/bg-3.jpg);">
                         <div class="catagory-content">
-                            <a href="#">Shoes</a>
+                            <a href="#">Partes de abajo</a>
                         </div>
                     </div>
                 </div>
-                <!-- Single Catagory -->
+                <!-- Single Category -->
                 <div class="col-12 col-sm-6 col-md-4">
                     <div class="single_catagory_area d-flex align-items-center justify-content-center bg-img" style="background-image: url(assets/img/bg-img/bg-4.jpg);">
                         <div class="catagory-content">
-                            <a href="#">Accessories</a>
+                            <a href="#">Zapatos</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Single Category -->
+                <div class="col-12 col-sm-6 col-md-4">
+                    <div class="single_catagory_area d-flex align-items-center justify-content-center bg-img" style="background-image: url(assets/img/bg-img/bg-4.jpg);">
+                        <div class="catagory-content">
+                            <a href="#">Accesorios</a>
                         </div>
                     </div>
                 </div>
@@ -275,7 +307,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="section-heading text-center">
-                        <h2>Popular Products</h2>
+                        <h2>Productos Populares</h2>
                     </div>
                 </div>
             </div>
@@ -286,6 +318,7 @@
                 <div class="col-12">
                     <div class="popular-products-slides owl-carousel">
 
+<<<<<<< Updated upstream:index.html
                         <!-- Single Product -->
                         <div class="single-product-wrapper">
                             <!-- Product Image -->
@@ -377,11 +410,51 @@
                                     <!-- Add to Cart -->
                                     <div class="add-to-cart-btn">
                                         <a href="#" class="btn essence-btn">Add to Cart</a>
+=======
+                        <?php
+
+                        global $dbconn;
+                        // Obtener todos los productos
+                        $result = $dbconn->query('SELECT * FROM products');
+
+                        // Verificar si hay productos
+                        if ($result->rowCount() > 0) {
+                            // Iterar sobre los productos y mostrar cada uno
+                            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                        ?>
+                                <!-- Single Product -->
+                                <div class="single-product-wrapper">
+                                    <!-- Product Image -->
+                                    <div class="product-img">
+                                        <img src="<?php echo $row["image"]; ?>" alt="">
+                                        <!-- Hover Thumb -->
+                                        <img class="hover-img" src="<?php echo $row["image"]; ?>" alt="">
+                                    </div>
+                                    <!-- Product Description -->
+                                    <div class="product-description">
+                                        <span><?php echo $row["brand"]; ?></span>
+                                        <a href="single-product-details.html">
+                                            <h6><?php echo $row["name"]; ?></h6>
+                                        </a>
+                                        <p class="product-price">$<?php echo $row["price"]; ?></p>
+
+                                        <!-- Hover Content -->
+                                        <div class="hover-content">
+                                            <!-- Add to Cart -->
+                                            <div class="add-to-cart-btn">
+                                                <a href="#" class="btn essence-btn">Add to Cart</a>
+                                            </div>
+                                        </div>
+>>>>>>> Stashed changes:index.php
                                     </div>
                                 </div>
-                            </div>
-                        </div>
+                        <?php
+                            }
+                        } else {
+                            echo "No hay productos disponibles.";
+                        }
 
+<<<<<<< Updated upstream:index.html
                         <!-- Single Product -->
                         <div class="single-product-wrapper">
                             <!-- Product Image -->
@@ -394,28 +467,13 @@
                                 <div class="product-badge new-badge">
                                     <span>New</span>
                                 </div>
+=======
+                        // Cerrar la conexión a la base de datos
+                        $conn->close();
+                        ?>
+>>>>>>> Stashed changes:index.php
 
-                                <!-- Favourite -->
-                                <div class="product-favourite">
-                                    <a href="#" class="favme fa fa-heart"></a>
-                                </div>
-                            </div>
-                            <!-- Product Description -->
-                            <div class="product-description">
-                                <span>mango</span>
-                                <a href="single-product-details.html">
-                                    <h6>PETITE Belted Jumper Dress</h6>
-                                </a>
-                                <p class="product-price">$80.00</p>
-
-                                <!-- Hover Content -->
-                                <div class="hover-content">
-                                    <!-- Add to Cart -->
-                                    <div class="add-to-cart-btn">
-                                        <a href="#" class="btn essence-btn">Add to Cart</a>
-                                    </div>
-                                </div>
-                            </div>
+                        
                         </div>
                     </div>
                 </div>
@@ -462,48 +520,26 @@
                     <div class="single_widget_area d-flex mb-30">
                         <!-- Logo -->
                         <div class="footer-logo mr-50">
+<<<<<<< Updated upstream:index.html
                             <a href="#"><img src="assets/img/core-img/logo2.png" alt=""></a>
+=======
+                            <a href="#"><img src="assets/img/core-img/logo.png" alt="" height="100px" width="100px"></a>
+>>>>>>> Stashed changes:index.php
                         </div>
                         <!-- Footer Menu -->
                         <div class="footer_menu">
                             <ul>
-                                <li><a href="shop.html">Shop</a></li>
-                                <li><a href="blog.html">Blog</a></li>
-                                <li><a href="contact.html">Contact</a></li>
+                                <li><a href="tops.php">Partes de arriba</a></li>
+                                <li><a href="bottom.php">Partes de abajo</a></li>
+                                <li><a href="shoes.php">Zapatos</a></li>
+                                <li><a href="accesories.php">Accesorios</a></li>
                             </ul>
                         </div>
-                    </div>
-                </div>
-                <!-- Single Widget Area -->
-                <div class="col-12 col-md-6">
-                    <div class="single_widget_area mb-30">
-                        <ul class="footer_widget_menu">
-                            <li><a href="#">Order Status</a></li>
-                            <li><a href="#">Payment Options</a></li>
-                            <li><a href="#">Shipping and Delivery</a></li>
-                            <li><a href="#">Guides</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Terms of Use</a></li>
-                        </ul>
                     </div>
                 </div>
             </div>
 
             <div class="row align-items-end">
-                <!-- Single Widget Area -->
-                <div class="col-12 col-md-6">
-                    <div class="single_widget_area">
-                        <div class="footer_heading mb-30">
-                            <h6>Subscribe</h6>
-                        </div>
-                        <div class="subscribtion_form">
-                            <form action="#" method="post">
-                                <input type="email" name="mail" class="mail" placeholder="Your email here">
-                                <button type="submit" class="submit"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
                 <!-- Single Widget Area -->
                 <div class="col-12 col-md-6">
                     <div class="single_widget_area">
@@ -518,12 +554,14 @@
                 </div>
             </div>
 
-<div class="row mt-5">
+            <div class="row mt-5">
                 <div class="col-md-12 text-center">
                     <p>
                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>, distributed by <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
-    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://github.com/SamuelG0319/Vanity_Shop.git" target="_blank">Keily Marín, Samuel Lasso, Miguel Rodríguez & Carlos Serrano</a>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     </p>
                 </div>
             </div>
