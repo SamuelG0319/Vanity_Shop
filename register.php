8b09d70a07aba91e1858894e6cd7a1d37a9707f5
Hey Issac <cisg1703@gmail.com>
2023-12-08 21:29:46 -0500

    Query created for new user

--- register.php
+++ register.php
@@ -11,20 +11,35 @@ if (isset($_POST['register'])) {
         $lastname = trim($_POST['lastname']);
         $username = trim($_POST['username']);
         $email = trim($_POST['email']);
-        $address = trim($_POST['$address']);
-        $phone = trim($_POST['$phone']);
-        $password = trim($_POST['$password']);
+        $address = trim($_POST['address']);
+        $phone = trim($_POST['phone']);
+        $password = trim($_POST['password']);
 
-        // if (
-        //     !empty($name) && !empty($lastname) && !empty($username) && !empty($email) && !empty($address) &&
-        //     !empty($phone) && !empty($password)
-        // ) {
-        //     $verifyUser = $dbconn->prepare("select user from user where user =: user");
-        //     $verifyUser->execute(['user' => $username]);
-        //     $selectedUser = $verifyUser->fetch(PDO::FETCH_ASSOC);
+        if (
+            !empty($name) && !empty($lastname) && !empty($username) && !empty($email) && !empty($address) &&
+            !empty($phone) && !empty($password)
+        ) {
+            $verifyUser = $dbconn->prepare("select user from user where user = :user");
+            $verifyUser->execute(['user' => $username]);
+            $selectedUser = $verifyUser->fetch(PDO::FETCH_ASSOC);
 
-        //     if($username == $selectedUser['user'])
-        // }
+            if ($username == $selectedUser['user']) {
+                '<script> alert("Usuario existente"); </script>';
+            } else {
+                $insertUser = "insert into user (user, password, name, last_name, email, address, phone) values (:user, :password, :name, :last_name, :email, :address, :phone)";
+                $prepareUser = $dbconn->prepare($insertUser);
+                $createUser = $prepareUser->execute(array(':user' => $username, ':password' => $password, ':name' => $name,
+                    ':last_name' => $lastname, ':email' => $email, ':address' => $address, ':phone' => $phone));
+
+                if ($createUser) {
+                    session_start();
+                    $_SESSION['user'] = $username;
+                    header("Location: indext.html");
+                } else {
+                    echo 'Llena todos los campos';
+                }
+            }
+        }
     }
 }
 ?>
@@ -47,8 +62,8 @@ if (isset($_POST['register'])) {
         <input type="email" placeholder="Correo electrónico" name="email" id="email" required><br><br>
         <input type="text" placeholder="Dirección" name="address" id="address" required><br><br>
         <input type="tel" placeholder="Teléfono" name="phone" id="phone" pattern="[0-9]{4}-[0-9]{4}" required><br>
-        <input type="password" placeholder="Contraseña" name="password" id="password" required><br><br>
         <small>Formato: 6123-4567</small><br><br>
+        <input type="password" placeholder="Contraseña" name="password" id="password" required><br><br>
         <input name="register" id="register" type="submit">
     </form>
 </body>
