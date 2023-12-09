d878f1e91d0d96d392f4a0c2bdb8f185c83c409c
Hey Issac <cisg1703@gmail.com>
2023-12-08 21:08:26 -0500

    Connecting 'register.php' to database

--- register.php
+++ register.php
@@ -4,9 +4,27 @@ global $dbconn;
 
 if (isset($_POST['register'])) {
     if (
-        isset($_POST['name']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['password'])
-        && isset($_POST['phone']) && isset($_POST['email'])) {
+        isset($_POST['name']) && isset($_POST['lastname']) && isset($_POST['username']) && isset($_POST['email']) &&
+        isset($_POST['phone']) && isset($_POST['address']) && isset($_POST['password'])
+    ) {
+        $name = trim($_POST['name']);
+        $lastname = trim($_POST['lastname']);
+        $username = trim($_POST['username']);
+        $email = trim($_POST['email']);
+        $address = trim($_POST['$address']);
+        $phone = trim($_POST['$phone']);
+        $password = trim($_POST['$password']);
 
+        // if (
+        //     !empty($name) && !empty($lastname) && !empty($username) && !empty($email) && !empty($address) &&
+        //     !empty($phone) && !empty($password)
+        // ) {
+        //     $verifyUser = $dbconn->prepare("select user from user where user =: user");
+        //     $verifyUser->execute(['user' => $username]);
+        //     $selectedUser = $verifyUser->fetch(PDO::FETCH_ASSOC);
+
+        //     if($username == $selectedUser['user'])
+        // }
     }
 }
 ?>
@@ -25,10 +43,11 @@ if (isset($_POST['register'])) {
     <form method="POST">
         <input type="text" placeholder="Nombre" name="name" id="name" required><br><br>
         <input type="text" placeholder="Apellido" name="lastname" id="lastname" required><br><br>
+        <input type="text" placeholder="Username" name="username" id="username" required><br><br>
         <input type="email" placeholder="Correo electrónico" name="email" id="email" required><br><br>
-        <input type="password" placeholder="Contraseña" name="password" id="password" required><br><br>
         <input type="text" placeholder="Dirección" name="address" id="address" required><br><br>
         <input type="tel" placeholder="Teléfono" name="phone" id="phone" pattern="[0-9]{4}-[0-9]{4}" required><br>
+        <input type="password" placeholder="Contraseña" name="password" id="password" required><br><br>
         <small>Formato: 6123-4567</small><br><br>
         <input name="register" id="register" type="submit">
     </form>
