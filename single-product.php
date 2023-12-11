67806811ebee3832ce4374a1ff6fc4f0f1d08564
Hey Issac <cisg1703@gmail.com>
2023-12-11 01:38:51 -0500

    Redirection page fixed

--- single-product.php
+++ single-product.php
@@ -98,6 +98,7 @@ if (isset($_SESSION['cod_user'])) {
         $removeItem = $dbconn->prepare($queryRemoveItem);
         $removeItem->execute(array(':product_code' => $productCodeToRemove, ':cod_user' => $cod_user));
         
+        header("Location: single-product.php?id=" . $productCode);
         $success = true;
         echo json_encode(['success' => $success]);
         exit;
